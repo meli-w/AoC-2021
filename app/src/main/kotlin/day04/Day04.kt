8c6fe0145ba5bbbea6909dab90f3381dea7a8792
melanie.weber <melanie.weber@wetteronline.de>
2021-12-09 14:16:28 +0100

    solve Day 4

--- app/src/main/kotlin/day04/Day04.kt
+++ app/src/main/kotlin/day04/Day04.kt
@@ -11,11 +11,140 @@ fun main(args: Array<String>) {
     solve(day, input, ::solveDay04Part1, ::solveDay04Part2)
 }
 
-
 fun solveDay04Part1(input: List<String>): Int {
-    TODO()
+    val bingoNumbers: List<Int> = getBingoNumber(input)
+    val bingoFields = getBingoField(input)
+
+    return checkWinning(bingoNumbers, bingoFields)
 }
 
 fun solveDay04Part2(input: List<String>): Int {
-    TODO()
+    val bingoNumbers: List<Int> = getBingoNumber(input)
+    val bingoFields = getBingoField(input)
+
+    return checkWinningPart2(bingoNumbers, bingoFields)
 }
+
+private fun checkWinning(
+    bingoNumbers: List<Int>,
+    bingoFields: List<Board>
+): Int {
+    bingoNumbers.forEach { number ->
+        bingoFields.forEach { board ->
+            board.checkNumber(number)
+            if (board.hasBingo()) {
+                return number * board.sumUnmarkedNumbers()
+            }
+        }
+    }
+    return 0
+}
+
+private fun checkWinningPart2(
+    bingoNumbers: List<Int>,
+    bingoFields: List<Board>
+): Int {
+    val mutableBingoFields: MutableList<Board> = bingoFields.toMutableList()
+    var winningCheckSum = 0
+
+    bingoNumbers.forEach { number ->
+        val winningBoards = mutableBingoFields.checkNumber(number)
+
+        if (winningBoards.isNotEmpty()) {
+            winningBoards
+                .reversed()
+                .forEach {
+                    mutableBingoFields.removeAt(it.boardIndex)
+                }
+
+            winningCheckSum = winningBoards.last().winningCheckSum
+        }
+    }
+    return winningCheckSum
+}
+
+private fun List<Board>.checkNumber(number: Int): List<WinningBoard> {
+    val winningBoards = mutableListOf<WinningBoard>()
+    forEachIndexed { index, board ->
+        board.checkNumber(number)
+        if (board.hasBingo()) {
+            winningBoards.add(
+                WinningBoard(
+                    winningCheckSum = number * board.sumUnmarkedNumbers(),
+                    boardIndex = index
+                )
+            )
+        }
+    }
+    return winningBoards
+}
+
+data class WinningBoard(val winningCheckSum: Int, val boardIndex: Int)
+
+class Board(val board: List<List<BingoNumber>>) {
+
+    fun checkNumber(number: Int) {
+        board.forEach { row: List<BingoNumber> ->
+            row.forEach { bingoNumber ->
+                if (number == bingoNumber.value) {
+                    bingoNumber.isChecked = true
+                }
+            }
+        }
+    }
+
+    fun hasBingo(): Boolean = bingoRow() || bingoColumn()
+
+    fun sumUnmarkedNumbers(): Int = board
+        .flatten()
+        .filter { !it.isChecked }
+        .sumOf { it.value }
+
+    private fun bingoColumn(): Boolean {
+        repeat(board.size) { columnIndex ->
+            board
+                .map { it[columnIndex] }
+                .let { column ->
+                    if (bingo(column)) {
+                        return true
+                    }
+                }
+        }
+        return false
+    }
+
+    private fun bingoRow(): Boolean {
+        board.forEach { row ->
+            if (bingo(row)) {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun bingo(bingoNumbers: List<BingoNumber>): Boolean = bingoNumbers.all(BingoNumber::isChecked)
+}
+
+data class BingoNumber(val value: Int, var isChecked: Boolean = false)
+
+fun getBingoNumber(input: List<String>): List<Int> = input
+    .first()
+    .split(",")
+    .map { it.toInt() }
+
+fun getBingoField(input: List<String>): List<Board> = input
+    .let { it.subList(2, it.size) }
+    .filterNot { it.isEmpty() }
+    .chunked(5)
+    .map { bingoField: List<String> ->
+        Board(
+            bingoField
+                .map { row ->
+                    row
+                        .replace("(\\s+)?(\\d+)".toRegex(), "$2,")
+                        .split(",")
+                        .filterNot { it.isEmpty() }
+                        .map { BingoNumber(it.toInt()) }
+                }
+        )
+    }
--- app/src/test/kotlin/day04/Day04Test.kt
+++ app/src/test/kotlin/day04/Day04Test.kt
@@ -6,13 +6,74 @@ import io.kotest.matchers.shouldBe
 class Day04Test : FreeSpec({
 
     val sampleInput: List<String> = listOf(
+        "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
+        "",
+        "22 13 17 11  0",
+        " 8  2 23  4 24",
+        "21  9 14 16  7",
+        " 6 10  3 18  5",
+        " 1 12 20 15 19",
+        "",
+        " 3 15  0  2 22",
+        " 9 18 13 17  5",
+        "19  8  7 25 23",
+        "20 11 10 24  4",
+        "14 21 16 12  6",
+        "",
+        "14 21 17 24  4",
+        "10 16 15  9 19",
+        "18  8 23 26 20",
+        "22 11 13  6  5",
+        " 2  0 12  3  7",
     )
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1: Int = 4512
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2: Int = 1924
 
+    val bingoPuzzle: List<Board> = listOf(
+        Board(
+            listOf(
+                listOf(BingoNumber(22), BingoNumber(13), BingoNumber(17), BingoNumber(11), BingoNumber(0)),
+                listOf(BingoNumber(8), BingoNumber(2), BingoNumber(23), BingoNumber(4), BingoNumber(24)),
+                listOf(BingoNumber(21), BingoNumber(9), BingoNumber(14), BingoNumber(16), BingoNumber(7)),
+                listOf(BingoNumber(6), BingoNumber(10), BingoNumber(3), BingoNumber(18), BingoNumber(5)),
+                listOf(BingoNumber(1), BingoNumber(12), BingoNumber(20), BingoNumber(15), BingoNumber(19)),
+            )
+        ),
+        Board(
+            listOf(
+                listOf(BingoNumber(3), BingoNumber(15), BingoNumber(0), BingoNumber(2), BingoNumber(22)),
+                listOf(BingoNumber(9), BingoNumber(18), BingoNumber(13), BingoNumber(17), BingoNumber(5)),
+                listOf(BingoNumber(19), BingoNumber(8), BingoNumber(7), BingoNumber(25), BingoNumber(23)),
+                listOf(BingoNumber(20), BingoNumber(11), BingoNumber(10), BingoNumber(24), BingoNumber(4)),
+                listOf(BingoNumber(14), BingoNumber(21), BingoNumber(16), BingoNumber(12), BingoNumber(6)),
+            )
+        ),
+        Board(
+            listOf(
+                listOf(BingoNumber(14), BingoNumber(21), BingoNumber(17), BingoNumber(24), BingoNumber(4)),
+                listOf(BingoNumber(10), BingoNumber(16), BingoNumber(15), BingoNumber(9), BingoNumber(19)),
+                listOf(BingoNumber(18), BingoNumber(8), BingoNumber(23), BingoNumber(26), BingoNumber(20)),
+                listOf(BingoNumber(22), BingoNumber(11), BingoNumber(13), BingoNumber(6), BingoNumber(5)),
+                listOf(BingoNumber(2), BingoNumber(0), BingoNumber(12), BingoNumber(3), BingoNumber(7)),
+            )
+        )
+    )
+
+    val bingoNumbers =
+        listOf<Int>(7, 4, 9, 5, 11, 17, 23, 2, 0, 14, 21, 24, 10, 16, 13, 6, 15, 25, 12, 22, 18, 20, 8, 19, 3, 26, 1)
     "Solving day 4" - {
+
+        "convert the bingoNumbers works correctly" {
+            getBingoNumber(sampleInput) shouldBe bingoNumbers
+        }
+
+        "convert the bingoField correctly" {
+            val bingoField: List<Board> = getBingoField(sampleInput)
+            bingoField[0].board shouldBe bingoPuzzle[0].board
+        }
+
         "part 1 for the sample input should return the correct output" {
             solveDay04Part1(sampleInput) shouldBe sampleSolutionPart1
         }
