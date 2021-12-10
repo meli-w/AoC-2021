5144607b8041300d5cb62c1fd1f15e3890ea73e0
melanie.weber <melanie.weber@wetteronline.de>
2021-12-10 17:17:47 +0100

    solve day 7

--- app/src/main/kotlin/day07/Day07.kt
+++ app/src/main/kotlin/day07/Day07.kt
@@ -3,6 +3,7 @@ package day07
 import common.InputRepo
 import common.readSessionCookie
 import common.solve
+import kotlin.math.abs
 
 fun main(args: Array<String>) {
     val day = 7
@@ -13,9 +14,60 @@ fun main(args: Array<String>) {
 
 
 fun solveDay07Part1(input: List<String>): Int {
-    TODO()
+    var lastPosition = 0
+    var useFuel = -1
+    input
+        .first()
+        .split(",")
+        .map { it.toInt() }
+        .sorted()
+        .let {
+            lastPosition = it.last()
+            it
+        }
+        .groupBy { it }
+        .mapValues { (_, value) -> value.size }
+        .let { map ->
+            repeat(lastPosition) { position ->
+                var fuel = 0
+                map.forEach { (innerPosition, count) ->
+                    fuel += abs((innerPosition - position) * count)
+                }
+                if (useFuel == -1 || useFuel > fuel) {
+                    useFuel = fuel
+                }
+            }
+        }
+
+    return useFuel
 }
 
 fun solveDay07Part2(input: List<String>): Int {
-    TODO()
+    var lastPosition = 0
+    var useFuel = -1
+    input
+        .first()
+        .split(",")
+        .map { it.toInt() }
+        .sorted()
+        .let {
+            lastPosition = it.last()
+            it
+        }
+        .groupBy { it }
+        .mapValues { (_, value) -> value.size }
+        .let { map ->
+            repeat(lastPosition) { position ->
+                var fuel = 0
+                map.forEach { (innerPosition, count) ->
+                    val steps = abs(innerPosition - position)
+                    fuel += (steps * (steps + 1)) / 2 * count
+                }
+                if (useFuel == -1 || useFuel > fuel) {
+                    useFuel = fuel
+                }
+            }
+        }
+
+    return useFuel
 }
--- app/src/test/kotlin/day07/Day07Test.kt
+++ app/src/test/kotlin/day07/Day07Test.kt
@@ -6,11 +6,12 @@ import io.kotest.matchers.shouldBe
 class Day07Test : FreeSpec({
 
     val sampleInput: List<String> = listOf(
+        "16,1,2,0,4,2,7,1,2,14"
     )
 
-    val sampleSolutionPart1: Int = -1
+    val sampleSolutionPart1: Int = 37
 
-    val sampleSolutionPart2: Int = -1
+    val sampleSolutionPart2: Int = 168
 
     "Solving day 7" - {
         "part 1 for the sample input should return the correct output" {
